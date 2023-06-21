45b73937f459558f2d9c4634b3e368881a14914f
Felipe Puentes <feliipepuentes@gmail.com>
2023-06-21 17:17:48 -0500

    solving challenge

--- Start/Regex/Challenge/Program.cs
+++ Start/Regex/Challenge/Program.cs
@@ -1,6 +1,7 @@
-﻿using System.Text.RegularExpressions;
+﻿// Some Description
+using System.Text.RegularExpressions;
 
-Regex dates = new(@"\d{2,}+");
+Regex dates = new(@"\d+");
 bool programIsRunning = true;
 
 while (programIsRunning)
@@ -8,9 +9,26 @@ while (programIsRunning)
     Console.WriteLine("Date to Convert? (Use mm/dd/yyy), or 'exit' to quit)");
     string? result = Console.ReadLine();
 
+    if (result == "exit")
+    {
+        programIsRunning = false;
+        break;
+    }
+
     if (result == null || !dates.IsMatch(result))
     {
         Console.WriteLine("Invalid Entry, try again");
         continue;
     }
+
+    MatchCollection mc = dates.Matches(result);
+    Console.WriteLine($"Found {mc.Count} matches:");
+
+    if (mc.Count < 3)
+    {
+        Console.WriteLine("Invalid Entry, try again");
+        continue;
+    }
+
+    Console.WriteLine($"{mc[2].Value}-{mc[0].Value}-{mc[1].Value}");
 }
